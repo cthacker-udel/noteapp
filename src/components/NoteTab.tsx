2123e0d02fa64dc6e804ea085c4a4fb719f7c59c
cthacker-udel <cthacker@udel.edu>
2021-10-06 08:07:16 -0400

    Added NoteTab module

---
+++ src/components/NoteTab.tsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Pagination } from 'react-bootstrap';
+
+export const NoteTab = (): JSX.Element =>
+	<>
+		<Pagination>
+			<></>
+		</Pagination>
+	</>;
+
