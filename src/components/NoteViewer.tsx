4aded8300cfa521f6866acb957800df953c25c53
cthacker-udel <cthacker@udel.edu>
2021-10-09 01:44:30 -0400

    updated3

--- src/components/NoteViewer.tsx
+++ src/components/NoteViewer.tsx
@@ -3,16 +3,14 @@ import { Form } from 'react-bootstrap';
 import { CurrentNoteText } from '../context/CurrentNoteTextContext';
 import React, { useState } from 'react';
 
-export const NoteViewer = (): JSX.Element => {
-	const [currText, setCurrText] = useState<string>('');
-	return (
-		<CurrentNoteText.Provider value={currText}>
-			<Form>
-				<Form.Group className="mb-3" controlId="noteviewerform">
-					<Form.Control as="textarea" placeholder="Enter text here" rows={3} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCurrText(e.target.value)}/>
-					<Form.Text style={{ textAlign: 'center', display: 'block' }}>The buttons to create, edit, and delete notes are below</Form.Text>
-				</Form.Group>
-			</Form>
-		</CurrentNoteText.Provider>
-	);
-};
+export const NoteViewer = (props: { onChange: React.Dispatch<React.SetStateAction<string>> }): JSX.Element =>
+	<Form>
+		<Form.Group className="mb-3" controlId="noteviewerform">
+			<Form.Control as="textarea" placeholder="Enter text here" rows={3} onChange={(e) => {
+				const htmlElement = e.target as HTMLInputElement;
+				props.onChange(htmlElement.value);
+			}}/>
+			<Form.Text style={{ textAlign: 'center', display: 'block' }}>The buttons to create, edit, and delete notes are below</Form.Text>
+		</Form.Group>
+	</Form>;
+
