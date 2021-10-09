070a5e0ec1cea40098ce1960ae4607477c9694f7
cthacker-udel <cthacker@udel.edu>
2021-10-09 01:42:10 -0400

    updated2

--- src/components/NoteViewer.tsx
+++ src/components/NoteViewer.tsx
@@ -1,23 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form } from 'react-bootstrap';
-<<<<<<< Updated upstream
-import React from 'react';
-
-export const NoteViewer = (props: { onChange: React.Dispatch<React.SetStateAction<string>> }): JSX.Element => {
-	const [tmpValue] = [0];
-	return (
-		<Form>
-			<Form.Group className="mb-3" controlId="noteviewerform">
-				<Form.Control as="textarea" placeholder="Enter text here" rows={3} onChange={(e) => {
-					const htmlElement = e.target as HTMLInputElement;
-					props.onChange(htmlElement.value);
-				}}/>
-				<Form.Text style={{ textAlign: 'center', display: 'block' }}>The buttons to create, edit, and delete notes are below</Form.Text>
-			</Form.Group>
-		</Form>);
-};
-
-=======
 import { CurrentNoteText } from '../context/CurrentNoteTextContext';
 import React, { useState } from 'react';
 
@@ -34,4 +16,3 @@ export const NoteViewer = (): JSX.Element => {
 		</CurrentNoteText.Provider>
 	);
 };
->>>>>>> Stashed changes
